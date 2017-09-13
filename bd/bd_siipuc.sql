7bd837e266745ce61fb0ef339b7533321f21fdd4
De16liz <deligodi1628@gmail.com>
2017-09-13 11:08:20 -0500

    Add files via upload

--- bd/bd_siipuc.sql
+++ bd/bd_siipuc.sql
@@ -1,1 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-09-2017 a las 01:50:11
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 5.6.20
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_siipuc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_comites`
+--
+
+CREATE TABLE `tb_comites` (
+  `codigo` int(10) NOT NULL,
+  `tipo_comite` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `eventos` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_eventos`
+--
+
+CREATE TABLE `tb_eventos` (
+  `codigo` int(10) NOT NULL,
+  `descripcion` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_feligres`
+--
+
+CREATE TABLE `tb_feligres` (
+  `Nro_folio` int(10) NOT NULL,
+  `id` varchar(11) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `nacimiento` date NOT NULL,
+  `EC` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` varchar(11) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `barrio` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_pastor`
+--
+
+CREATE TABLE `tb_pastor` (
+  `id` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `Nro_licencia` varchar(11) COLLATE utf8_spanish_ci NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `correo` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `nacimiento` date NOT NULL,
+  `telefono` varchar(11) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `barrio` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_registro`
+--
+
+CREATE TABLE `tb_registro` (
+  `codigo` int(10) NOT NULL,
+  `Rg_pastor` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Rg_feligrez` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_reportes`
+--
+
+CREATE TABLE `tb_reportes` (
+  `codigo` int(10) NOT NULL,
+  `certificados` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `membresias` varchar(80) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_usuarios`
+--
+
+CREATE TABLE `tb_usuarios` (
+  `nombres` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo_usuario` varchar(1) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tb_comites`
+--
+ALTER TABLE `tb_comites`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `tb_eventos`
+--
+ALTER TABLE `tb_eventos`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `tb_feligres`
+--
+ALTER TABLE `tb_feligres`
+  ADD PRIMARY KEY (`Nro_folio`);
+
+--
+-- Indices de la tabla `tb_pastor`
+--
+ALTER TABLE `tb_pastor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tb_registro`
+--
+ALTER TABLE `tb_registro`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `tb_reportes`
+--
+ALTER TABLE `tb_reportes`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD PRIMARY KEY (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tb_comites`
+--
+ALTER TABLE `tb_comites`
+  MODIFY `codigo` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tb_eventos`
+--
+ALTER TABLE `tb_eventos`
+  MODIFY `codigo` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tb_feligres`
+--
+ALTER TABLE `tb_feligres`
+  MODIFY `Nro_folio` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tb_registro`
+--
+ALTER TABLE `tb_registro`
+  MODIFY `codigo` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tb_reportes`
+--
+ALTER TABLE `tb_reportes`
+  MODIFY `codigo` int(10) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
