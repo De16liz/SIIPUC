736df629b9dcf352822c385e4f48ff70643901ec
De16liz <deligodi1628@gmail.com>
2017-10-24 21:55:01 -0500

    Add files via upload

---
+++ comites.php
@@ -0,0 +1,60 @@
+<?php include( "funciones.php" ); ?>
+
+<!-- Esto es para la tabla de pastor. -->
+
+<html>
+	<head>
+		<?php include('includes/headgo.php'); ?>
+		<title>Ministros/pastor</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/estilos.css">
+		<link rel="stylesheet"  href="css/fonts.css">
+		<script src="js/jssor.slider.min.js"></script>
+	
+	</head>
+
+	<body>
+	<div class='col-xs-12 col-md-4 col-lg-4 ' >
+					
+	</div>
+	<div class='col-xs-12 col-md-4 col-lg-4 ' >
+		<img src="img/ipuc.png">			
+	</div>
+	<div class='col-xs-12 col-md-4 col-lg-4 ' >
+					
+	</div>
+	
+		<main>
+		<article>
+		<?php  echo imprimir_menus();  ?>
+		<br>
+
+		<!--<form action="guardar_datos.php" method="get">
+			Documento			<input type="text" name="dato1">
+			Licencia	 			<input type="text" name="dato2"><br><br>
+			Nombre 	<input type="text" name="dato3">
+			Correo 	<input type="text" name="dato4">
+			Fecha nacimiento 	<input type="text" name="dato5"><br><br>
+			Telefono 	<input type="text" name="dato6">
+			Direccion 	<input type="text" name="dato7">
+			Barrio 	<input type="text" name="dato8">
+			<input type="hidden" name="tabla" value="tb_pastor">
+			<input type="hidden" name="total-campos" value="8">
+			<input type="hidden" name="campos" value="id, Nro_licencia, Nombre, nacimiento, telefono, direccion, barrio">
+			<input type="submit" value="Enviar">
+		</form><br><br>-->
+		<center>
+		<?php 
+			echo traer_informacion( "tb_comites" ); //Trae los datos. 
+		?>
+		</center>
+		</main>
+		</article>
+<?php include('includes/footer.php'); ?>
+		<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/headroom.min.js"></script>
+	<script type="text/javascript" src="js/menu.js"></script>
+
+	</body>
+
+</html>
--- config.php
+++ config.php
@@ -1,8 +1,15 @@
  <?php  
 /*conexion a base de datos*/
-$conexion=  new mysqli ('localhost', 'root', '' ,'bd_siipuc');
-if ($conexion->connect_error) {
-	die('Error en la conexion' . $conexion->connect_error);
-}
+$servidor = "localhost";
+$usuario = "root";
+$clave = "";
+$bd = "bd_siipuc";
 
+$conexion= mysqli_connect ($servidor, $usuario, $clave ,$bd);
+//$resultado= $conexion->query($sql);
+//$conexion=  new mysqli ('localhost', 'root', '' ,'bd_siipuc');
+//if ($conexion->connect_error) {
+	//die('Error en la conexion' . $conexion->connect_error);
+//}
+$sn_pruebas = "n";
 ?>
